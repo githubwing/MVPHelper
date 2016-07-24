fa51dbcc93f356a3b3999b1eb5646ef6385aae02
wing <615555755@qq.com>
2016-07-25 00:03:24 +0800

    the first version

---
+++ src/com/wingsofts/mvphelper/ClassCreateHelper.java
@@ -0,0 +1,85 @@
+package com.wingsofts.mvphelper;
+
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.DataKeys;
+import com.intellij.openapi.vfs.VirtualFile;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**创建class的类
+ * Created by wing on 2016/7/23.
+ */
+public class ClassCreateHelper {
+   static final int  MODEL = 0;
+    static final int  PRESENTER = 1;
+    public static void CreateClass(String path,String className,String classFullName,int mode) throws IOException {
+        String type = null;
+        if(mode == MODEL){
+            type = "Model";
+        }else {
+            type = "Presenter";
+        }
+        String dir = path+type.toLowerCase()+"/";
+        path = dir+className+type+"Impl.java";
+        File dirs = new File(dir);
+        File file = new File(path);
+        String packageName = getPackageName(path);
+        System.out.println(packageName);
+        dirs.mkdirs();
+        file.createNewFile();
+        BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+        writer.write("package "+ packageName + type.toLowerCase()+";");
+
+        writer.newLine();
+        writer.write("import "+packageName +"contract."+classFullName+";");
+        writer.newLine();
+        writer.newLine();
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("YYYY/MM/dd");
+        writer.write("/**\n* Created by MVPHelper on "+sdf.format(date)+"\n*/");
+
+        writer.newLine();
+        writer.newLine();
+        writer.write("public class "+className+type+"Impl implements "+
+                classFullName+"."+className+type+"{");
+        writer.newLine();
+
+        writer.newLine();
+        writer.write("}");
+        writer.flush();
+        writer.close();
+    }
+
+    private static String getPackageName(String path) {
+        String[] strings = path.split("/");
+        StringBuilder packageName = new StringBuilder();
+        int index = 0;
+        int length = strings.length;
+        for(int i = 0;i<strings.length;i++){
+            if(strings[i].equals("com")||strings[i].equals("org")||strings.equals("cn")){
+                index = i;
+                break;
+            }
+
+        }
+        for(int j = index;j<length-2;j++){
+            packageName.append(strings[j]+".");
+        }
+        return packageName.toString();
+    }
+
+    public static String getCurrentPath(AnActionEvent e,String classFullName){
+
+        VirtualFile currentFile = DataKeys.VIRTUAL_FILE.getData(e.getDataContext());
+
+        String path = currentFile.getPath().replace(classFullName+".java","");
+        return path;
+    }
+
+
+}
---
+++ src/com/wingsofts/mvphelper/MVPHelperAction.java
@@ -0,0 +1,144 @@
+package com.wingsofts.mvphelper;
+
+import com.intellij.openapi.actionSystem.*;
+import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
+
+import java.io.IOException;
+
+/**
+ * 程序的入口
+ * Created by wing on 16/7/23.
+ */
+public class MVPHelperAction extends AnAction {
+    private ClassModel _classModel;
+    private Editor _editor;
+    private String _content;
+    private boolean canCreate;
+    private AnActionEvent _event;
+    private String _path;
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+
+        this._event = e;
+        canCreate = true;
+        init(e);
+        getClassModel();
+        createFiles();
+
+        try {
+            if(canCreate) {
+                createClassFiles();
+                MessagesCenter.showMessage("created success! please wait a moment\n生成成功,稍后请刷新左侧src文件夹!","success");
+                refreshProject(e);
+            }
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
+
+    }
+
+    private void refreshProject(AnActionEvent e) {
+        e.getProject().getBaseDir().refresh(false,true);
+        }
+
+    /**
+     * 创建class文件
+     * create class files
+     * @throws IOException
+     */
+    private void createClassFiles() throws IOException {
+        String className = _classModel.get_className();
+        String classFullName = _classModel.get_classFullName();
+        System.out.println("_path:" + _path);
+            ClassCreateHelper.CreateClass(_path
+                    , className
+                    , classFullName, ClassCreateHelper.MODEL
+            );
+            ClassCreateHelper.CreateClass(
+                    _path, className
+                    , classFullName, ClassCreateHelper.PRESENTER
+            );
+
+        }
+
+
+    /**
+     * 生成 contract类内容
+     * create Contract Model Presenter
+     */
+    private void createFiles() {
+        if (null == _classModel.get_className()) {
+            return;
+        }
+
+         _path= ClassCreateHelper.getCurrentPath(_event,_classModel.get_classFullName());
+        if(_path.contains("contract")) {
+            _path = _path.replace("contract/", "");
+        }else {
+            MessagesCenter.showErrorMessage("Your Contract should in package 'contract'\n你的Contract类应该在contract包中","error");
+            canCreate = false;
+        }
+        if(canCreate) {
+            setFileDocument();
+        }
+
+    }
+
+    /**
+     * 生成 contract类内容
+     * create Contract Model Presenter
+     */
+    private void setFileDocument() {
+
+
+        int lastIndex = _content.lastIndexOf("}");
+        _content = _content.substring(0, lastIndex);
+        MessagesCenter.showDebugMessage(_content, "debug");
+        String content = setContent();
+        //wirte in runWriteAction
+        WriteCommandAction.runWriteCommandAction(_editor.getProject(),
+                () -> _editor.getDocument().setText(content));
+
+    }
+
+    private String setContent() {
+        String className = _classModel.get_className();
+        String content = _content + "public interface " + className + "View{\n}\n\n"
+                + "public interface " + className + "Presenter{\n}\n\n"
+                + "public interface " + className + "Model{\n}\n\n"
+                + "\n}";
+
+        return content;
+    }
+
+
+    private void getClassModel() {
+        _content = _editor.getDocument().getText();
+
+        String[] words = _content.split(" ");
+
+        for (String word : words) {
+            if (word.contains("Contract")) {
+                String className = word.substring(0, word.indexOf("Contract"));
+                _classModel.set_className(className);
+                _classModel.set_classFullName(word);
+                MessagesCenter.showDebugMessage(className, "class name");
+            }
+        }
+        if (null == _classModel.get_className()) {
+            MessagesCenter.showErrorMessage("Create failed ,Can't found 'Contract' in your class name,your class name must contain 'Contract'\n生成失败！你的类名必须以'Contract'为后缀！", "error");
+            canCreate = false;
+        }
+    }
+
+    private void init(AnActionEvent e) {
+        _editor = e.getData(PlatformDataKeys.EDITOR);
+        _classModel = new ClassModel();
+    }
+
+
+}
